5cd3c630351eb462dc78c6635769ef013b0fd8d8
Richie Basile <132868419+Baznigol@users.noreply.github.com>
2023-12-26 20:27:52 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,7 @@ export default function Page() {
                   title={project.title}
                   description={project.description}
                   tags={project.techStack}
-                  link={"link" in project ? project.link.href : undefined}
+                  link={"link" in project ? (project.link as any).href : undefined}
                 />
               );
             })}
